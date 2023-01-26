30728e1652cba7804f0747b617b8f2feb18211d1
Hidenori Miyoshi <miyohide@gmail.com>
2023-01-26 20:45:04 +0900

    create initial project

---
+++ src/main/java/com/github/miyohide/my_java_template/MyJavaTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.my_java_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJavaTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJavaTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/miyohide/my_java_template/MyJavaTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.my_java_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJavaTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
