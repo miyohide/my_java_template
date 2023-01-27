b59a8f6a090aeb59833dea4dcd1eb8a13e5f5b7b
Hidenori Miyoshi <miyohide@gmail.com>
2023-01-27 21:08:25 +0900

    create domain

---
+++ src/main/java/com/github/miyohide/my_java_template/Member.java
@@ -0,0 +1,22 @@
+package com.github.miyohide.my_java_template;
+
+import org.springframework.data.annotation.Id;
+
+public class Member {
+    @Id
+    private final Integer id;
+    private final String name;
+
+    public Member(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
