a6d5607c7683294e1a9f15414466c25e1e3a4d7d
Hidenori Miyoshi <miyohide@gmail.com>
2023-01-28 14:24:21 +0900

    update test

--- src/test/java/com/github/miyohide/my_java_template/MemberRepositoryTest.java
+++ src/test/java/com/github/miyohide/my_java_template/MemberRepositoryTest.java
@@ -13,11 +13,14 @@ class MemberRepositoryTest {
 
   @Test
   void test() {
-    String name = "みよひで";
+    String name = "foobar";
     Member save = memberRepository.save(new Member(null, name));
+    // 保存することでidが採番されてnullではなくなること
     assertNotNull(save.getId());
     Optional<Member> maybeMember = memberRepository.findById(save.getId());
+    // 保存したデータを検索したときに正常に検索できること
     assertTrue(maybeMember.isPresent());
+    // 検索で得られたデータが保存したデータと一致していること
     maybeMember.ifPresent(member -> assertEquals(save.getName(), member.getName()));
   }
 }
