a7b34eec76716b4e33fa785708dd4a548ecebe18
Hidenori Miyoshi <miyohide@gmail.com>
2023-01-28 17:20:00 +0900

    change table name

--- src/main/java/com/github/miyohide/my_java_template/Member.java
+++ src/main/java/com/github/miyohide/my_java_template/Member.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @RequiredArgsConstructor
 @Getter
 @EqualsAndHashCode(of = {"id"})
 @ToString
+@Table(name = "MEMBERS")
 public class Member {
   @Id private final Integer id;
   private final String name;
--- src/main/resources/db/migration/V002__insert_members.sql
+++ src/main/resources/db/migration/V002__insert_members.sql
@@ -1,5 +1,5 @@
-insert into MEMBER (NAME) values ('test member 001');
-insert into MEMBER (NAME) values ('test member 002');
-insert into MEMBER (NAME) values ('test member 003');
-insert into MEMBER (NAME) values ('test member 004');
-insert into MEMBER (NAME) values ('test member 005');
+insert into MEMBERS (NAME) values ('test member 001');
+insert into MEMBERS (NAME) values ('test member 002');
+insert into MEMBERS (NAME) values ('test member 003');
+insert into MEMBERS (NAME) values ('test member 004');
+insert into MEMBERS (NAME) values ('test member 005');
