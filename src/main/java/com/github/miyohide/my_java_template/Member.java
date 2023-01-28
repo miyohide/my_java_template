d9b4819119240d7b785e9c6100a2d1b1ab92c0c4
Hidenori Miyoshi <miyohide@gmail.com>
2023-01-28 14:10:02 +0900

    use Lombok

--- src/main/java/com/github/miyohide/my_java_template/Member.java
+++ src/main/java/com/github/miyohide/my_java_template/Member.java
@@ -1,21 +1,16 @@
 package com.github.miyohide.my_java_template;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 
+@RequiredArgsConstructor
+@Getter
+@EqualsAndHashCode(of = {"id"})
+@ToString
 public class Member {
   @Id private final Integer id;
   private final String name;
-
-  public Member(Integer id, String name) {
-    this.id = id;
-    this.name = name;
-  }
-
-  public Integer getId() {
-    return id;
-  }
-
-  public String getName() {
-    return name;
-  }
 }
