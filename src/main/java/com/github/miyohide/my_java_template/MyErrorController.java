7730fd7e113178e44179160e68f07ff6b34f4eb7
Hidenori Miyoshi <miyohide@gmail.com>
2023-02-06 20:52:14 +0900

    enhance handleError method

--- src/main/java/com/github/miyohide/my_java_template/MyErrorController.java
+++ src/main/java/com/github/miyohide/my_java_template/MyErrorController.java
@@ -1,6 +1,9 @@
 package com.github.miyohide.my_java_template;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,23 @@ import java.time.LocalDateTime;
 @Controller
 public class MyErrorController implements ErrorController {
     @RequestMapping("/error")
-    public String handleError(Model model) {
+    public String handleError(Model model, HttpServletRequest request) {
+        // エラー画面に表示する日時情報を設定
         model.addAttribute("errorDateTime", LocalDateTime.now());
+        // HTTPステータスコードの取得
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                // 404（NOT FOUND）の場合
+                return "errors/error-404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                // 500（INTERNAL SERVER ERROR）の場合
+                return "errors/error-500";
+            }
+        }
         return "errors/error";
     }
 }
