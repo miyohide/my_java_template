7f0861bde558d02041127f0e4aac0a10b726638f
Hidenori Miyoshi <miyohide@gmail.com>
2023-02-08 06:51:29 +0900

    implement new/create controller

--- src/main/java/com/github/miyohide/my_java_template/MemberController.java
+++ src/main/java/com/github/miyohide/my_java_template/MemberController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -20,6 +22,19 @@ public class MemberController {
   public String show(@PathVariable("id") Integer id, Model model) {
     // TODO memberが存在しない時の処理を実装する
     model.addAttribute("member", memberRepository.findById(id).get());
+    return "members/show";
+  }
+
+  @GetMapping("/members/new")
+  public String newMember(Model model) {
+    model.addAttribute("member", new Member());
+    return "members/new";
+  }
+
+  @PostMapping("/members")
+  public String create(@ModelAttribute Member member, Model model) {
+    Member savedMember = memberRepository.save(member);
+    model.addAttribute("member", savedMember);
     return "members/show";
   }
 }
