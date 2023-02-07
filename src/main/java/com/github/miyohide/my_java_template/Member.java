ab4af860d6066137180670f00b693ba56827de04
Hidenori Miyoshi <miyohide@gmail.com>
2023-02-07 20:53:10 +0900

    add default contractor and setter

--- src/main/java/com/github/miyohide/my_java_template/Member.java
+++ src/main/java/com/github/miyohide/my_java_template/Member.java
@@ -1,18 +1,17 @@
 package com.github.miyohide.my_java_template;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
+@Setter
 @EqualsAndHashCode(of = {"id"})
 @ToString
 @Table(name = "MEMBERS")
 public class Member {
-  @Id private final Integer id;
-  private final String name;
+  @Id private Integer id;
+  private String name;
 }
