988350132d839d68ac4b2310317165886f9fb31e
Hidenori Miyoshi <miyohide@gmail.com>
2023-02-11 08:37:02 +0900

    add tomcat logging config

---
+++ src/main/java/com/github/miyohide/my_java_template/TomcatLoggingConfig.java
@@ -0,0 +1,16 @@
+package com.github.miyohide.my_java_template;
+
+import ch.qos.logback.access.tomcat.LogbackValve;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatLoggingConfig {
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        tomcatServletWebServerFactory.addContextValves(new LogbackValve());
+        return tomcatServletWebServerFactory;
+    }
+}
