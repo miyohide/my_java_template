c107fe7beb233a94ff4efde4af952f528ebd6bdf
Hidenori Miyoshi <miyohide@gmail.com>
2023-04-22 17:54:48 +0900

    create first integration test

---
+++ src/test/java/com/github/miyohide/my_java_template/MemberControllerIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.github.miyohide.my_java_template;
+
+import static org.hamcrest.Matchers.containsString;
+
+import com.github.miyohide.my_java_template.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+    classes = MyJavaTemplateApplication.class)
+@AutoConfigureMockMvc
+public class MemberControllerIntegrationTest {
+  @Autowired private MockMvc mvc;
+  @Autowired private MemberRepository repository;
+
+  @Test
+  void test() throws Exception {
+    mvc.perform(MockMvcRequestBuilders.get("/"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string(containsString("All members page")));
+  }
+}
