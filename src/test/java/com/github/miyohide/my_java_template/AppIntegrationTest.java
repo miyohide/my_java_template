abc3bcf4d6b26adf7bea616034bed0dd1ebb2594
Hidenori Miyoshi <miyohide@gmail.com>
2023-04-22 18:02:21 +0900

    refactoring. rename class name.

--- src/test/java/com/github/miyohide/my_java_template/AppIntegrationTest.java
+++ src/test/java/com/github/miyohide/my_java_template/AppIntegrationTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
     webEnvironment = SpringBootTest.WebEnvironment.MOCK,
     classes = MyJavaTemplateApplication.class)
 @AutoConfigureMockMvc
-public class MemberControllerIntegrationTest {
+public class AppIntegrationTest {
   @Autowired private MockMvc mvc;
 
   @Test
