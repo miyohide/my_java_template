6a5419bd9c6c32f6b6c29c6bb74feb0680eda1dd
Hidenori Miyoshi <miyohide@gmail.com>
2023-06-24 21:02:31 +0900

    greetingのエンドポイントを実装

--- src/main/java/com/github/miyohide/my_java_template/GreetingController.java
+++ src/main/java/com/github/miyohide/my_java_template/GreetingController.java
@@ -1,18 +1,21 @@
 package com.github.miyohide.my_java_template;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@RestController
+@Controller
 public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", new Greeting(counter.incrementAndGet(), String.format(template, name)).toString());
+        return "greeting";
     }
 }
