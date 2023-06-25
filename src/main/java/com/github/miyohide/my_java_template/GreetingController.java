7028b43f76ab4e44df2cb357fe7a06359fbdaa9e
Hidenori Miyoshi <miyohide@gmail.com>
2023-06-25 20:44:53 +0900

    run spotlessApply

--- src/main/java/com/github/miyohide/my_java_template/GreetingController.java
+++ src/main/java/com/github/miyohide/my_java_template/GreetingController.java
@@ -1,7 +1,6 @@
 package com.github.miyohide.my_java_template;
 
 import java.util.concurrent.atomic.AtomicLong;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
