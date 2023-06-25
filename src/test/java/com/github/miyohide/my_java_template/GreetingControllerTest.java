1fc7bd0f97a827b7cf791ce68571f39e4c6844b3
Hidenori Miyoshi <miyohide@gmail.com>
2023-06-25 20:44:39 +0900

    run spotlessApply

--- src/test/java/com/github/miyohide/my_java_template/GreetingControllerTest.java
+++ src/test/java/com/github/miyohide/my_java_template/GreetingControllerTest.java
@@ -1,5 +1,7 @@
 package com.github.miyohide.my_java_template;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,16 +10,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 public class GreetingControllerTest {
   @Test
   void returnOkandRenderGreetingPageWhengreeting(@Autowired MockMvc mvc) throws Exception {
-    mvc.perform(
-            MockMvcRequestBuilders.get("/greeting"))
-            .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(view().name("greeting"));
+    mvc.perform(MockMvcRequestBuilders.get("/greeting"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(view().name("greeting"));
   }
 }
