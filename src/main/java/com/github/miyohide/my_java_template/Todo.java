d5baf31ec73ab541204ec4a4134c94e04aaae399
Hidenori Miyoshi <miyohide@gmail.com>
2023-06-28 21:20:13 +0900

    create entity file

---
+++ src/main/java/com/github/miyohide/my_java_template/Todo.java
@@ -0,0 +1,18 @@
+package com.github.miyohide.my_java_template;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@RequiredArgsConstructor
+@Getter
+@EqualsAndHashCode(of = {"id"})
+@ToString
+@Table(name = "todos")
+public class Todo {
+  @With
+  @Id
+  private final Long id;
+  private final String title;
+  private final String body;
+}
