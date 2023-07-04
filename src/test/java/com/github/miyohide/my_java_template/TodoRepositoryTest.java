214e2b63745fac76bcb17919927b285d7d6925ad
Hidenori Miyoshi <miyohide@gmail.com>
2023-07-04 20:09:33 +0900

    add test

--- src/test/java/com/github/miyohide/my_java_template/TodoRepositoryTest.java
+++ src/test/java/com/github/miyohide/my_java_template/TodoRepositoryTest.java
@@ -15,10 +15,24 @@ public class TodoRepositoryTest {
 
   @Test
   void test() {
+    // 新規作成
+    var before_insert_count = todoRepository.count();
     Todo t1 = todoRepository.save(new Todo(null, "title1", "body1"));
     assertNotNull(t1.getId());
+    // 新規作成のため、件数は増えること
+    assertEquals(before_insert_count + 1, todoRepository.count());
+    // 検索
     Optional<Todo> t2 = todoRepository.findById(t1.getId());
     assertTrue(t2.isPresent());
     t2.ifPresent(todo -> assertEquals(t1.getTitle(), todo.getTitle()));
+    // 更新
+    var before_update_count = todoRepository.count();
+    Todo t3 = new Todo(t1.getId(), "updated title", "updated body");
+    Todo updatedTodo = todoRepository.save(t3);
+    // 更新のため、件数は増えないこと
+    assertEquals(before_update_count, todoRepository.count());
+    // 更新されていることの確認
+    assertEquals(t3.getTitle(), updatedTodo.getTitle());
+    assertEquals(t3.getBody(), updatedTodo.getBody());
   }
 }
