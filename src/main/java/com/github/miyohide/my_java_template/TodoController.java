1a2427ad14de7c44f477b20bdfc75344e460d915
Hidenori Miyoshi <miyohide@gmail.com>
2023-07-05 21:20:19 +0900

    新しくtodoを作る処理を実装

--- src/main/java/com/github/miyohide/my_java_template/TodoController.java
+++ src/main/java/com/github/miyohide/my_java_template/TodoController.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TodoController {
@@ -21,5 +23,17 @@ public class TodoController {
     Iterable<Todo> todos = todoService.getAllTodo();
     model.addAttribute("todos", todos);
     return "todos/index";
+  }
+
+  @GetMapping("/todos/new")
+  public String newTodo(Model model) {
+    model.addAttribute("todo", new Todo(null, "", ""));
+    return "todos/new";
+  }
+
+  @PostMapping("/todo")
+  public String createTodo(@ModelAttribute Todo todo, Model model) {
+    todoService.saveTodo(todo);
+    return "redirect:" + "/todos";
   }
 }
