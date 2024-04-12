23211ab8162280a904a53b7c3c2a2107995fa5ff
Hidenori Miyoshi <miyohide@gmail.com>
2024-04-12 21:04:12 +0900

    lombokを使ってloggerを実装する

--- src/main/java/com/github/miyohide/my_java_template/GreetingController.java
+++ src/main/java/com/github/miyohide/my_java_template/GreetingController.java
@@ -1,16 +1,16 @@
 package com.github.miyohide.my_java_template;
 
 import java.util.concurrent.atomic.AtomicLong;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class GreetingController {
-  private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
   private static final String template = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
 
