1ef8a659883a484a9f859c04647960df3243f84d
Hidenori Miyoshi <miyohide@gmail.com>
2024-04-24 20:52:45 +0900

    TodoControllerTestを生成

---
+++ src/test/java/com/github/miyohide/my_java_template/TodoControllerTest.java
@@ -0,0 +1,25 @@
+package com.github.miyohide.my_java_template;
+
+import org.junit.jupiter.api.Test;
+
+public class TodoControllerTest {
+    @Test
+    void testCreateTodo() {
+
+    }
+
+    @Test
+    void testDetails() {
+
+    }
+
+    @Test
+    void testIndex() {
+
+    }
+
+    @Test
+    void testNewTodo() {
+
+    }
+}
