67d9fde4f3ecbeda50d010983a81e7c7fdf416c8
Hidenori Miyoshi <miyohide@gmail.com>
2024-04-25 21:07:31 +0900

    アノテーション付与

--- src/test/java/com/github/miyohide/my_java_template/TodoControllerTest.java
+++ src/test/java/com/github/miyohide/my_java_template/TodoControllerTest.java
@@ -1,7 +1,11 @@
 package com.github.miyohide.my_java_template;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TodoControllerTest {
     @Test
     void testCreateTodo() {
