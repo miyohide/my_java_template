d2474f44a404490c9fa3ebe8df72dbe552910682
Hidenori Miyoshi <miyohide@gmail.com>
2024-04-26 20:36:11 +0900

    Todoテストを一つ実装

--- src/test/java/com/github/miyohide/my_java_template/TodoControllerTest.java
+++ src/test/java/com/github/miyohide/my_java_template/TodoControllerTest.java
@@ -1,15 +1,23 @@
 package com.github.miyohide.my_java_template;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TodoControllerTest {
     @Test
-    void testCreateTodo() {
-
+    void testCreateTodo(@Autowired MockMvc mvc) throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/todos"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(view().name("todos/index"));
     }
 
     @Test
