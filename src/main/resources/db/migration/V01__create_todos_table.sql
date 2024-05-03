2cf6476ac13daff68bccfcc3b4f919aeb6dbf05b
Hidenori Miyoshi <miyohide@gmail.com>
2024-05-03 21:27:47 +0900

    user_idとcompletedカラムを追加

--- src/main/resources/db/migration/V01__create_todos_table.sql
+++ src/main/resources/db/migration/V01__create_todos_table.sql
@@ -1,5 +1,8 @@
 CREATE TABLE todos (
     id BIGSERIAL PRIMARY KEY,
     title varchar(100) NOT NULL,
-    body varchar(1000) NOT NULL
+    body varchar(1000) NOT NULL,
+    user_id BIGINT NOT NULL,
+    completed boolean NOT NULL DEFAULT false,
+    foreign key (user_id) references users(id)
 );
