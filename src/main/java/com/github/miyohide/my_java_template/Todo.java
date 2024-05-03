38abf1ded3c68d317e166c60e4ffd1c2e0273dc2
Hidenori Miyoshi <miyohide@gmail.com>
2024-05-03 21:58:10 +0900

    追加したカラムをmodelに追加

--- src/main/java/com/github/miyohide/my_java_template/Todo.java
+++ src/main/java/com/github/miyohide/my_java_template/Todo.java
@@ -13,4 +13,6 @@ public class Todo {
   @With @Id private final Long id;
   private final String title;
   private final String body;
+  private final Long userId;
+  private final boolean completed;
 }
