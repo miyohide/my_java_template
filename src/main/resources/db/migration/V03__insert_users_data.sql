12904fb0b7c2f7db2e150f27eb0c95a80a8dc9ec
Hidenori Miyoshi <miyohide@gmail.com>
2024-05-04 18:00:36 +0900

    create initial data

---
+++ src/main/resources/db/migration/V03__insert_users_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name, number_of_todos) VALUES ('test user 001', 0);
+INSERT INTO users (name, number_of_todos) VALUES ('test user 002', 0);
+INSERT INTO users (name, number_of_todos) VALUES ('test user 003', 0);
