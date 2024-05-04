7344bfeb1302838c5535c2a0992e8774f86f0ca6
Hidenori Miyoshi <miyohide@gmail.com>
2024-05-04 12:03:38 +0900

    fix syntax error

--- src/main/resources/db/migration/V01__create_users_table.sql
+++ src/main/resources/db/migration/V01__create_users_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
-    name varchar(100) NOT NULL
-    number_of_todos LONG NOT NULL DEFAULT 0
+    name varchar(100) NOT NULL,
+    number_of_todos BIGINT NOT NULL DEFAULT 0
 );
