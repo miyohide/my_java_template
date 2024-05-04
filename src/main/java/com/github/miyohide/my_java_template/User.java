b7dfa3fb067823e43cf6d609071dfd3f9e21c55a
Hidenori Miyoshi <miyohide@gmail.com>
2024-05-04 11:55:25 +0900

    ユーザーを作成

---
+++ src/main/java/com/github/miyohide/my_java_template/User.java
@@ -0,0 +1,19 @@
+package com.github.miyohide.my_java_template;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@Getter
+@EqualsAndHashCode(of = {"id"})
+@ToString
+@Table(name = "users")
+public class User {
+    @Id private final Long id;
+    private final String name;
+}
