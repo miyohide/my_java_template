8aa913d4007e3d0ae6e7c5b84f87801c5b0582da
Hidenori Miyoshi <miyohide@gmail.com>
2024-05-04 18:01:35 +0900

    userのMapを作成するメソッドを追加

---
+++ src/main/java/com/github/miyohide/my_java_template/UserService.java
@@ -0,0 +1,26 @@
+package com.github.miyohide.my_java_template;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Map<String, Long> getUserMap() {
+        Iterable<User> users = userRepository.findAll();
+        Map<String, Long> rval = new LinkedHashMap<>();
+
+        for (User user : users) {
+            rval.put(user.getName(), user.getId());
+        }
+
+        return rval;
+    }
+}
