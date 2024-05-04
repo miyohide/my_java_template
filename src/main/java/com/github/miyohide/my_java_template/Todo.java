887a75ac678fe7ce4b61539adb44e17497b56c79
Hidenori Miyoshi <miyohide@gmail.com>
2024-05-04 12:29:47 +0900

    finalを削除

--- src/main/java/com/github/miyohide/my_java_template/Todo.java
+++ src/main/java/com/github/miyohide/my_java_template/Todo.java
@@ -4,15 +4,15 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
-@RequiredArgsConstructor
-@Getter
+@AllArgsConstructor
+@Data
 @EqualsAndHashCode(of = {"id"})
 @ToString
 @Table(name = "todos")
 public class Todo {
-  @Id private final Long id;
-  private final String title;
-  private final String body;
-  private final Long userId;
-  private final boolean completed;
+  @Id private Long id;
+  private String title;
+  private String body;
+  private Long userId;
+  private boolean completed;
 }
