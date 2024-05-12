a639e23f5a8d705fab6aaf9665387bdfd1518f80
Hidenori Miyoshi <miyohide@gmail.com>
2024-05-12 13:49:09 +0900

    MySQL用にSQL文を修正

--- src/main/resources/db/migration/V01__create_users_table.sql
+++ src/main/resources/db/migration/V01__create_users_table.sql
@@ -1,5 +1,13 @@
 CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGINT NOT NULL AUTO_INCREMENT,
     name varchar(100) NOT NULL,
-    number_of_todos BIGINT NOT NULL DEFAULT 0
+    number_of_todos BIGINT NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
 );
+
+-- for PostgreSQL
+-- CREATE TABLE users (
+--     id BIGSERIAL PRIMARY KEY,
+--     name varchar(100) NOT NULL,
+--     number_of_todos BIGINT NOT NULL DEFAULT 0,
+-- );
--- src/main/resources/db/migration/V02__create_todos_table.sql
+++ src/main/resources/db/migration/V02__create_todos_table.sql
@@ -1,8 +1,19 @@
 CREATE TABLE todos (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGINT NOT NULL AUTO_INCREMENT,
     title varchar(100) NOT NULL,
     body varchar(1000) NOT NULL,
     user_id BIGINT NOT NULL,
     completed boolean NOT NULL DEFAULT false,
-    foreign key (user_id) references users(id)
+    foreign key (user_id) references users(id),
+    PRIMARY KEY (id)
 );
+
+-- for PostgreSQL
+-- CREATE TABLE todos (
+--     id BIGSERIAL PRIMARY KEY,
+--     title varchar(100) NOT NULL,
+--     body varchar(1000) NOT NULL,
+--     user_id BIGINT NOT NULL,
+--     completed boolean NOT NULL DEFAULT false,
+--     foreign key (user_id) references users(id),
+-- );
