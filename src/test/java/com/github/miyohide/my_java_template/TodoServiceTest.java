2825c53428c512c70d883980fa70f1ddc2feedc9
Hidenori Miyoshi <miyohide@gmail.com>
2024-05-14 21:08:56 +0900

    TodoService用のテストのテンプレートを作成

---
+++ src/test/java/com/github/miyohide/my_java_template/TodoServiceTest.java
@@ -0,0 +1,24 @@
+package com.github.miyohide.my_java_template;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoServiceTest {
+
+  @Test
+  void getAllTodo() {
+  }
+
+  @Test
+  void getTodoById() {
+  }
+
+  @Test
+  void createTodo() {
+  }
+
+  @Test
+  void deleteTodo() {
+  }
+}
