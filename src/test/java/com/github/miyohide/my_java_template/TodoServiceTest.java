3b77182d2bddab6ee6e066f720a554c2686f4c9d
Hidenori Miyoshi <miyohide@gmail.com>
2024-05-16 20:02:30 +0900

    不要なimportを削除

--- src/test/java/com/github/miyohide/my_java_template/TodoServiceTest.java
+++ src/test/java/com/github/miyohide/my_java_template/TodoServiceTest.java
@@ -1,7 +1,6 @@
 package com.github.miyohide.my_java_template;
 
 import com.google.common.collect.Iterables;
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
