fc126dcd8fc5eb82a57d65588c78aa91f62d8d66
Hidenori Miyoshi <miyohide@gmail.com>
2024-05-16 20:00:22 +0900

    検証事項をより詳細にした

--- src/test/java/com/github/miyohide/my_java_template/TodoServiceTest.java
+++ src/test/java/com/github/miyohide/my_java_template/TodoServiceTest.java
@@ -1,6 +1,8 @@
 package com.github.miyohide.my_java_template;
 
 import com.google.common.collect.Iterables;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -10,6 +12,7 @@ import org.mockito.MockitoAnnotations;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -31,12 +34,15 @@ class TodoServiceTest {
   @Test
   void getAllTodo() {
     List<Todo> todos = new ArrayList<>();
-    todos.add(new Todo(1L, "title1", "body1", 1L, true));
-    todos.add(new Todo(2L, "title2", "body2", 2L, true));
+    Todo todo1 = new Todo(1L, "title1", "body1", 1L, true);
+    Todo todo2 = new Todo(2L, "title2", "body2", 2L, true);
+    todos.add(todo1);
+    todos.add(todo2);
     when(todoRepository.findAllByOrderById()).thenReturn(todos);
 
     var results = todoService.getAllTodo();
     assertEquals(2, Iterables.size(results));
+    assertThat(results, Matchers.contains(todo1, todo2));
     assertAll(() -> {
       verify(todoRepository, times(1));
     });
