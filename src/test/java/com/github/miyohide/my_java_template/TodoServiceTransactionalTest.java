1539c2ebc3e200d0d8748d785ad867d9a3ecc6ad
Hidenori Miyoshi <miyohide@gmail.com>
2024-05-27 16:30:42 +0900

    不要なimportを削除

--- src/test/java/com/github/miyohide/my_java_template/TodoServiceTransactionalTest.java
+++ src/test/java/com/github/miyohide/my_java_template/TodoServiceTransactionalTest.java
@@ -6,13 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doThrow;
