14df1a69b54e4c8aa3b844eabda73b77560eb48a
Hidenori Miyoshi <miyohide@gmail.com>
2024-06-02 20:30:17 +0900

    validationのアサーションを追加

--- src/main/java/com/github/miyohide/my_java_template/Todo.java
+++ src/main/java/com/github/miyohide/my_java_template/Todo.java
@@ -1,5 +1,6 @@
 package com.github.miyohide.my_java_template;
 
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
@@ -11,7 +12,9 @@ import org.springframework.data.relational.core.mapping.Table;
 @Table(name = "todos")
 public class Todo {
   @Id private Long id;
+  @Size(min = 1, max = 50)
   private String title;
+  @Size(min = 1, max = 1000)
   private String body;
   private Long userId;
   private boolean completed;
