022e3a109676497e22e143ac5d96f8cf715e6883
Hidenori Miyoshi <miyohide@gmail.com>
2024-06-15 20:32:36 +0900

    不要なコメントを削除

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE IF NOT EXISTS `devdb`;
 CREATE DATABASE IF NOT EXISTS `testdb`;
 
--- CREATE USER 'root'@'localhost' IDENTIFIED BY 'local';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
