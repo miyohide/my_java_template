6d9273e2158a72e15e447d0f18371e5e26f1c334
Hidenori Miyoshi <miyohide@gmail.com>
2024-06-18 05:35:07 +0900

    テストコード見直し

--- src/test/java/com/github/miyohide/my_java_template/TodoControllerTest.java
+++ src/test/java/com/github/miyohide/my_java_template/TodoControllerTest.java
@@ -2,6 +2,13 @@ package com.github.miyohide.my_java_template;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,26 +20,48 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TodoControllerTest {
+    @Autowired
+    private DataSource dataSource;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        try (
+                Connection connection = dataSource.getConnection();
+                Statement statement = connection.createStatement()) {
+            statement.execute("INSERT INTO users VALUE (999, 'test user 999', 0)");
+            statement.execute("INSERT INTO todos VALUES (999, 'title', 'body', 999, false)");
+        }
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        try (
+                Connection connection = dataSource.getConnection();
+                Statement statement = connection.createStatement();) {
+            statement.execute("DELETE FROM users WHERE id = 999");
+            statement.execute("DELETE FROM todos WHERE id = 999");
+        }
+    }
+
     @Test
     void testCreateTodo(@Autowired MockMvc mvc) throws Exception {
-        // TODO この部分のテストは修正する必要あり
         mvc.perform(MockMvcRequestBuilders.get("/todos"))
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(view().name("todos/index"));
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(view().name("todos/index"));
     }
 
     @Test
     void testDetails(@Autowired MockMvc mvc) throws Exception {
-        // mvc.perform(MockMvcRequestBuilders.get("/todos/100"))
-        // .andExpect(MockMvcResultMatchers.status().isOk())
-        // .andExpect(view().name("todos/show"));
+        mvc.perform(MockMvcRequestBuilders.get("/todos/999"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(view().name("todos/show"));
     }
 
     @Test
     void testIndex(@Autowired MockMvc mvc) throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/todos"))
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(view().name("todos/index"));
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(view().name("todos/index"));
     }
 
     @Test
